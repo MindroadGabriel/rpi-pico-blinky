a5d608bca35a3906374111e6daedf35804ff1202
Gabriel Nilsson <gabriel.nilsson@mindroad.se>
2025-03-08 18:06:24 +0100

    Unstable: Run display driver

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,19 @@
 #![no_std]
 #![no_main]
 
+mod print;
+
+use core::cell::RefCell;
 use bsp::entry;
 use defmt::*;
 use defmt_rtt as _;
 use embedded_hal::digital::OutputPin;
 use panic_probe as _;
+
+// use rp2040_boot2;
+// #[link_section = ".boot2"]
+// #[used]
+// pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
@@ -21,36 +29,40 @@ use bsp::hal::{
     sio::Sio,
     watchdog::Watchdog,
 };
+use nano_rust_drivers::{error, ssd1306, ssd1306_registers};
+use nano_rust_drivers::ssd1306_registers::{BLACK, WHITE};
+use rp2040_hal::fugit::RateExtU32;
+use rp2040_hal::I2C;
 
 #[entry]
 fn main() -> ! {
     info!("Program start");
-    let mut pac = pac::Peripherals::take().unwrap();
+    let mut peripherals = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
+    let mut watchdog = Watchdog::new(peripherals.WATCHDOG);
+    let sio = Sio::new(peripherals.SIO);
 
     // External high-speed crystal on the pico board is 12Mhz
     let external_xtal_freq_hz = 12_000_000u32;
     let clocks = init_clocks_and_plls(
         external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
+        peripherals.XOSC,
+        peripherals.CLOCKS,
+        peripherals.PLL_SYS,
+        peripherals.PLL_USB,
+        &mut peripherals.RESETS,
         &mut watchdog,
     )
-    .ok()
-    .unwrap();
+        .ok()
+        .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
     let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
+        peripherals.IO_BANK0,
+        peripherals.PADS_BANK0,
         sio.gpio_bank0,
-        &mut pac.RESETS,
+        &mut peripherals.RESETS,
     );
 
     // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
@@ -63,15 +75,162 @@ fn main() -> ! {
     // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
     // in series with the LED.
     let mut led_pin = pins.led.into_push_pull_output();
+    //
+    // loop {
+    //     info!("on!");
+    //     led_pin.set_high().unwrap();
+    //     delay.delay_ms(2000);
+    //     info!("off!");
+    //     led_pin.set_low().unwrap();
+    //     delay.delay_ms(2000);
+    // }
 
-    loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(2000);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(2000);
+    // let dp = Peripherals::take().unwrap();
+    // let pins = arduino_hal::pins!(dp);
+    // let mut serial = arduino_hal::default_serial!(dp, pins, 115200);
+    // print::put_console(serial);
+
+    // let result = (|| -> Result<(), error::UDisplayError<arduino_hal::i2c::Error>> {
+    //     let mut i2c = arduino_hal::I2c::new(
+    //         dp.TWI,
+    //         pins.a4.into_pull_up_input(),
+    //         pins.a5.into_pull_up_input(),
+    //         50000,
+    //     );
+    //     let mut led = pins.d13.into_output();
+    //     let mut button1 = pins.d5.into_pull_up_input();
+    //     let mut button2 = pins.d4.into_pull_up_input();
+    //     let mut button1_last_pressed = button1.is_low();
+    //     let mut button2_last_pressed = button2.is_low();
+    //     let i2c_ref_cell = RefCell::new(i2c);
+    //     let mut accelerometer = bmi160::Driver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, None)?;
+    //     let mut buffer = [0x00; ssd1306::BUFFER_SIZE];
+    //     let display_result = ssd1306::DisplayDriver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, &mut buffer);
+    //     // print::print_type_name(&display_result);
+    //     //
+    //     // core::result::Result<
+    //     // hackathon_pong_controller::ssd1306::DisplayDriver<embedded_hal_bus::i2c::refcell::RefCellDevice<avr_hal_generic::i2c::I2c<atmega_hal::Atmega,
+    //     // avr_device::devices::atmega328p::TWI,
+    //     // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
+    //     // atmega_hal::port::PC4>,
+    //     // avr_hal_generic::port::Pin<avr_hal_generic::port::mode::Input,
+    //     // atmega_hal::port::PC5>,
+    //     // avr_hal_generic::clock::MHz16>>>,
+    //     // hackathon_pong_controller::ssd1306_error::Error<avr_hal_generic::i2c::Error>>
+    //     let mut display = match display_result {
+    //         Ok(display) => {
+    //             display
+    //         }
+    //         Err(error) => {
+    //             return Err(error.into());
+    //         }
+    //     };
+    //
+    //     // println!("BMI160 initialized");
+    //     info!("Started");
+
+    // loop {
+    //     // let button1_pressed = button1.is_low();
+    //     // let button2_pressed = button2.is_low();
+    //     // if button1_pressed && !button1_last_pressed {
+    //     //     println!("Button1 pressed");
+    //     // }
+    //     // if button2_pressed && !button2_last_pressed {
+    //     //     println!("Button2 pressed");
+    //     // }
+    //     // button1_last_pressed = button1_pressed;
+    //     // button2_last_pressed = button2_pressed;
+    //     //
+    //     // led.toggle();
+    //     // accelerometer.update()?;
+    //     // if let Some(output_data) = accelerometer.get_output_data() {
+    //     // }
+    //     println!("On");
+    //     display.fill_screen(ssd1306_registers::WHITE);
+    //     display.display()?;
+    //     led.set_high();
+    //     arduino_hal::delay_ms(2000);
+    //
+    //     println!("Off");
+    //     display.fill_screen(ssd1306_registers::BLACK);
+    //     display.display()?;
+    //     led.set_low();
+    //     arduino_hal::delay_ms(2000);
+    // }
+    let result = (|| -> Result<(), error::UDisplayError<bsp::hal::i2c::Error>> {
+        let mut i2c = I2C::i2c0(
+            peripherals.I2C0,
+            pins.gpio20.reconfigure(), // sda
+            pins.gpio21.reconfigure(), // scl
+            50.kHz(),
+            &mut peripherals.RESETS,
+            125_000_000.Hz(),
+        );
+        let i2c_ref_cell = RefCell::new(i2c);
+        let mut display_buffer = [0x00; ssd1306::BUFFER_SIZE];
+        let display_result = ssd1306::DisplayDriver::new(embedded_hal_bus::i2c::RefCellDevice::new(&i2c_ref_cell), None, &mut display_buffer);
+        let mut display = match display_result {
+            Ok(display) => {
+                display
+            }
+            Err(error) => {
+                return Err(error.into());
+            }
+        };
+        loop {
+            display.fill_screen(BLACK);
+            display.display()?;
+            delay.delay_ms(1000);
+            display.fill_screen(WHITE);
+            display.display()?;
+            delay.delay_ms(1000);
+            // display.fill_screen(ssd1306_registers::BLACK);
+            // for i in 0..32 {
+            //     display.draw_pixel(i, i, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(31 + i, 31, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(63 + i, 31 - i, WHITE)?;
+            // }
+            // for i in 0..32 {
+            //     display.draw_pixel(95 + i, 0, WHITE)?;
+            // }
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(4, 4, 57, 25, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(57, 4, 4, 25, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(97, 15, 44, 30, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_line(44, 15, 97, 30, WHITE)?;
+            // display.display()?;
+            // delay.delay_ms(1000);
+            // display.draw_fill_rect(4, 4, 57 - 4, 25 - 4, WHITE)?;
+            // display.display()?;
+            // // display.dim(true)?;
+            // delay.delay_ms(1000);
+            // // display.dim(false)?;
+            // delay.delay_ms(1000);
+            //
+            // display.clear_display();
+            // display.set_cursor(0, 0);
+            // // display.draw_string("You're a mean one,\nMister Grinch!");
+            // display.draw_string(" Abc");
+            // display.display()?;
+            // delay.delay_ms(2000);
+        }
+    })();
+    if let Err(error) = result {
+        info!("Error");
+        // info!("Error: {}", error);
     }
+    loop {}
 }
 
 // End of file
